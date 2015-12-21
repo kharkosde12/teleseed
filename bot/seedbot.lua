77e8a7fdb13ea1227d183b4cbf02bbfcd9d20f6c
kharkosde12 <kharkosde12@gmail.com>
2015-12-22 00:17:47 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {178263682,123567896,0,tonumber(our_id)},--Sudo users
+    sudo_users = {178263682,171716359,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
